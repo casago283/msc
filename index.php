b79f154adea871960cde02e71d4f9df8a624dde4
casago283 <casago283@gmail.com>
2018-04-11 14:12:10 -0600

    actualizacion de mensajes

--- index.php
+++ index.php
@@ -10,17 +10,13 @@ $p = new page();
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
         <title>Mark2</title>
     </head>
     <body class="container">
-
         <div >
             <form  action="index.php" method="post">
-
                 <div class="form-group">
                     <label for="tipo">Tipo</label>
                     <select class="form-control" name="tipo" id="tipo" >
@@ -28,38 +24,37 @@ $p = new page();
                             <option<?php echo ( isset($_POST['tipo']) && ($_POST['tipo']) == $key ) ? ' selected="selected"' : ""; ?> ><?php echo $key ?></option>
                         <?php } ?>
                     </select>
-
                     <label for="notaInicial">Nota Inicial</label>
-
                     <select class="form-control" name="notaInicial" id="notaInicial" selected="<?php echo (isset($_POST['notaInicial'])) ? $_POST['notaInicial'] : ""; ?>">
-
                         <option >Grados</option>
                         <?php foreach ($p->getCromatica2() as $key => $value) { ?>
                             <option  <?php echo ( isset($_POST['notaInicial']) && ($_POST['notaInicial']) == $value) ? ' selected="selected"' : ""; ?> > <?php echo $value; ?></option>
                         <?php } ?>
                     </select>
-
                     <label for="notaFinal">NotaFinal</label>
                     <select class="form-control" name="notaFinal" id="notaFinal" selected="<?php echo (isset($_POST['notaFinal'])) ? $_POST['notaFinal'] : ""; ?>">
-
                         <?php foreach ($p->getCromatica2() as $key => $value) { ?>
                             <option  <?php echo ( isset($_POST['notaFinal']) && ($_POST['notaFinal']) == $value) ? ' selected="selected"' : ""; ?> > <?php echo $value; ?></option>
                         <?php } ?>
                     </select>
-
                     <label for="notas">Notas</label>
                     <textarea class="form-control" name="notas" id="notas" rows="4"><?php echo (isset($_POST['notas'])) ? $_POST['notas'] : ""; ?></textarea>
-
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
             </br>
+            <?php if (!$p->getDatosValidos()) { ?>
             <div class="row">
-
-                <?php if (count($p->getNotas()) > 1) { ?>                
+                <div class="col-sm-4">
+                    <div class="alert alert-warning" role="alert">
+                        <?php echo $p->getMensaje() ?>
+                    </div>
+                </div>
+            </div>
+             <?php } ?>
+            <div class="row">
+                <?php if (count($p->getNotas()) > 0 && $p->getDatosValidos()) { ?>                
                     <?php foreach ($p->getCifrado() as $key => $value) { ?>    
-
-
                         <div class="col-sm-4">
                             <div class="card">
                                 <div class="card-body">
@@ -70,14 +65,13 @@ $p = new page();
                                 </div>
                             </div>
                         </div>
-
-
                     <?php } ?>
                 <?php } ?>
             </div> 
             <br>
 
             <?php if (isset($_POST['tipo'])) { ?>
+
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="card">
@@ -85,7 +79,6 @@ $p = new page();
                                 <h5 class="card-title">Ejemplo</h5>
                                 <p class="card-text">Notas:   C D E F G A</p>
                                 <p class="card-text">Grados:  1 2 3 4 5 6</p>
-
                             </div>
                         </div>
                     </div>
@@ -103,19 +96,13 @@ $p = new page();
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Escala de <?php echo $p->getNotaInicial() . " y " . $p->getNotaFinal() . " " . $p->getTipo() ?></h5>
-
-
                                 <?php foreach ($p->getEscala() as $key => $nota) { ?>
-
                                     <?php $key = $key + 1; ?>
-
                                     <?php echo $nota . "=" . $key . ","; ?>
                                 <?php } ?>
                                 <br>
                                 <?php foreach ($p->getEscalaTemp() as $key => $nota) { ?>
-
                                     <?php $key = $key + 1; ?>
-
                                     <?php echo $nota . "=" . $key . ","; ?>
                                 <?php } ?>
                             </div>
@@ -125,10 +112,6 @@ $p = new page();
                 </div>
                 <br>
                 <div class="row">
-
-
-
-
                 <?php } ?>
             </div>
 
--- page.php
+++ page.php
@@ -2,16 +2,20 @@
 
 class page {
 
-    private $notas=[];
+    private $notas = [];
+    private $notasO = [];
     private $notaInicial;
     private $notaFinal;
     private $tipo;
+    private $mensaje;
+    private $datosValidos;
     private $cifrado = [];
     private $cromatica = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
     private $cromatica2 = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
     private $cromaticaAsociada = ['C' => 1, 'C#' => 2, 'D' => 3, 'D#' => 4, 'E' => 5, 'F' => 6, 'F#' => 7, 'G' => '8', 'G#' => '9', 'A' => 10, 'A#' => 11, 'B' => 12];
     private $escalas = ["mayor" => ['1', '1', '1/2', '1', '1', '1', '1/2'],
-        "menor" => ['1', '1/2', '1', '1', '1/2', '1', '1']
+        "menor" => ['1', '1/2', '1', '1', '1/2', '1', '1'],
+        "mayor + b" => ['1', '1/2', '1/2', '1/2', '1', '1', '1', '1/2']
     ];
     private $escala = [];
     private $tercera = [];
@@ -25,6 +29,8 @@ class page {
             $this->notaInicial = $_POST['notaInicial'];
             $this->notaFinal = $_POST['notaFinal'];
             $this->tipo = $_POST['tipo'];
+            $this->mensaje = "";
+            $this->datosValidos = true;
             $indiceNotaRaiz = $this->cromaticaAsociada[$this->notaFinal] - 1;
             $salto = $indiceNotaRaiz;
             foreach ($this->escalas[$this->tipo] as $key => $value) {
@@ -36,9 +42,11 @@ class page {
                     $salto = $salto + 1;
                 }
             }
-            $this->notas = explode(' ', $this->notas);
+            $this->notasO = $this->notas;
+            $this->notas = explode(' ', $this->notasO);
+            $this->notasO = explode(' ', $this->notasO);
 
-            if (isset($_POST['notaInicial']) && $_POST['notaInicial'] != 'Grados') {
+            if (isset($_POST['notaInicial']) && $_POST['notaInicial'] != 'Grados' && $this->sonDatosValidos()) {
                 $salto = $this->cromaticaAsociada[$this->notaInicial] - 1;
                 $this->escalaTemp2 = [];
                 foreach ($this->escalas[$this->tipo] as $key => $value) {
@@ -55,8 +63,10 @@ class page {
                 }
                 $cuentaTemp = 0;
                 foreach ($this->notas as $key => $value) {
-                    $this->notas[$cuentaTemp] = $this->escalaTemp2[$value];
-                    $cuentaTemp = $cuentaTemp + 1;
+                    if (isset($this->notas[$cuentaTemp]) && isset($this->escalaTemp2[$value])) {
+                        $this->notas[$cuentaTemp] = $this->escalaTemp2[$value];
+                        $cuentaTemp = $cuentaTemp + 1;
+                    }
                 }
                 $this->escala2 = $this->escalaTemp;
             }
@@ -68,20 +78,65 @@ class page {
             $this->cifrado = ["primera" => [], "tercera" => [], "quinta" => []];
             $this->tercera = [];
             $this->quinta = [];
-            if (count($this->notas) > 0) {
+            if (count($this->notas) > 0 && $this->sonDatosValidos()) {
                 foreach ($this->notas as $key2 => $value) {
                     $nota = explode('-', $value);
-       
+                    if (isset($this->escala2[(int) $nota[0] - 1]) && $this->escala2[(int) $nota[0] + 1] && $this->escala2[(int) $nota[0] + 3]) {
                         $nota1 = ['nota' => $this->escala2[(int) $nota[0] - 1], 'posicion' => (count($nota) > 2) ? $nota[1] : 0, 'duracion' => (count($nota) > 2) ? $nota[2] : 0];
                         array_push($this->cifrado["primera"], $nota1);
                         $nota3 = ['nota' => $this->escala2[(int) $nota[0] + 1], 'posicion' => (count($nota) > 2) ? $nota[1] : 0, 'duracion' => (count($nota) > 2) ? $nota[2] : 0];
                         array_push($this->cifrado["tercera"], $nota3);
                         $nota5 = ['nota' => $this->escala2[(int) $nota[0] + 3], 'posicion' => (count($nota) > 2) ? $nota[1] : 0, 'duracion' => (count($nota) > 2) ? $nota[2] : 0];
                         array_push($this->cifrado["quinta"], $nota5);
-                    
+                    }
                 }
             }
         }
+    }
+
+    public function sonDatosValidos() {
+        
+        foreach ($this->notasO as $key => $value) {
+
+            if (count($this->notasO) == 1 && $value == "") {
+                $this->mensaje = "No ha ingresado datos";
+                $this->datosValidos = false;
+
+                break;
+            }
+            if ($this->notaInicial == 'Grados' && !ctype_digit($value)) {
+                $this->mensaje = "Los valores ingresados no son n&uacute;meros";
+                $this->datosValidos = false;
+
+                break;
+            }
+            if ($this->notaInicial != 'Grados' && !$this->esNotaValida($value)) {
+                $this->mensaje = "Los valores ingresados no son notas";
+                $this->datosValidos = false;
+
+                break;
+            }
+        }
+        return $this->datosValidos;
+    }
+
+    private function esNotaValida($nota) {
+
+        if (isset($this->cromaticaAsociada[strtoupper($nota)])) {
+            echo "si";
+            return true;
+        } else {
+            echo "no";
+            return false;
+        }
+    }
+
+    public function getDatosValidos() {
+        return $this->datosValidos;
+    }
+
+    public function getMensaje() {
+        return $this->mensaje;
     }
 
     public function getEscalaTemp() {
